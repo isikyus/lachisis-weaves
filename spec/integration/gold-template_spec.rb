19713ffd8b26dbeea20b387be27aeacf3b79935d
Isikyus <isikyus@users.noreply.github.com>
2022-08-21 13:45:44 +1000

    [incomplete] Add gold-template spec for layouts
    This spec is deliberately missing the fixtures at the moment,
    as I need to get approval to use the characters mentioned in
    the text first.

---
+++ spec/integration/gold-template_spec.rb
@@ -0,0 +1,36 @@
+require 'open3'
+
+RSpec.describe 'read_events.rb' do
+  LACHISIS_PATH = File.dirname(File.dirname(__dir__))
+
+  def generate_with_args(*args)
+    output, error, status = Open3.capture3(
+      'bundle',
+      'exec',
+      'ruby',
+      File.join(LACHISIS_PATH, 'read_events.rb'),
+      *args
+    )
+
+    # TODO: can't test this yet without a -q option or similar
+    #expect(error).to be_empty
+    expect(status).to eq 0
+
+    output
+  rescue RSpec::Expectations::ExpectationNotMetError => e
+    raise e.exception("#{e.message}\nError output was:\n\n#{error}")
+  end
+
+  context 'with known input and output' do
+    describe 'so-called "annealed" layout' do
+      let(:annealed_svg) do
+        File.open('spec/fixtures/kathrakopolis-annealed.svg', &:read)
+      end
+
+      specify 'generates a known-good layout' do
+        svg = generate_with_args('-s', 'spec/fixtures/kathrakopolis.xml')
+        expect(svg).to eq(annealed_svg)
+      end
+    end
+  end
+end
